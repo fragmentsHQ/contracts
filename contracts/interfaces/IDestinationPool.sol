5331b1cc61fa57f9845ef87efb7e9c2fee5c125a
Priyanshu Kumar Panda <79529908+Priyanshu1609@users.noreply.github.com>
2023-07-30 10:59:02 +0530

    mainnet-testing

--- contracts/interfaces/IDestinationPool.sol
+++ contracts/interfaces/IDestinationPool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPLv3
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
 interface IDestinationPool {
     function receiveFlowMessage(address, int96, uint256, uint256) external;
